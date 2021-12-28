56fd8d99faa05e08b686db748e693170c32a0122
Manuel Serret <mserret99@gmail.com>
2021-12-28 18:21:43 +0100

    push

---
+++ src/lib/ProjectBox.svelte
@@ -0,0 +1,30 @@
+<script>
+	export let imageLink;
+	export let link;
+	export let name;
+</script>
+
+<div class="project">
+	<div class="image">
+		<img src={imageLink} />
+	</div>
+	<a href={link}>
+		<span>{name}</span>
+	</a>
+	<div>
+		<slot />
+	</div>
+</div>
+
+<style>
+	.project {
+		background-color: #333;
+		border-radius: 10px;
+		width: 32%;
+		padding: 10px;
+		margin: 5px 0;
+	}
+
+	.image {
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,33 @@
-asd asd
+<script>
+	import ProjectBox from '$lib/ProjectBox.svelte';
+</script>
+
+<div>
+	<ProjectBox
+		imageLink="/nopcommerce-patches.jpg"
+		link="https://nopcommerce-patches.serret.dev/"
+		name="NopCommerce Patch Generator"
+	>
+		Generate Git patch files for NopCommerce releases, by selecting the releases or commits you want
+		to generate a patch for.
+	</ProjectBox>
+
+	<ProjectBox imageLink="/pdfsvex.jpg" link="https://pdfsvex.serret.dev/" name="pdfsvex">
+		PDF generation, made easy
+	</ProjectBox>
+	<ProjectBox
+		imageLink="/gleisbelegung.jpg"
+		link="https://gleisbelegung.serret.dev/"
+		name="Plugin Gleisbelegung (for stellwerksim.de)"
+	>
+		Plugin to display the occupancy of the individual platforms at a station
+	</ProjectBox>
+</div>
+
+<style>
+	div {
+		display: flex;
+		justify-content: space-around;
+		flex-wrap: wrap;
+	}
+</style>
